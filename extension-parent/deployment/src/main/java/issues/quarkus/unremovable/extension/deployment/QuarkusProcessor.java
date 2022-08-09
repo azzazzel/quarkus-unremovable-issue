35b149b1eda58f5c33e1aae5e3bd55f53f838425
Milen Dyankov <milendyankov@gmail.com>
2022-08-09 16:01:15 +0200

    Use `UnremovableBeanBuildItem` as a workaround

--- extension-parent/deployment/src/main/java/issues/quarkus/unremovable/extension/deployment/QuarkusProcessor.java
+++ extension-parent/deployment/src/main/java/issues/quarkus/unremovable/extension/deployment/QuarkusProcessor.java
@@ -1,8 +1,13 @@
 package issues.quarkus.unremovable.extension.deployment;
 
 import io.quarkus.arc.deployment.AutoAddScopeBuildItem;
+import io.quarkus.arc.deployment.UnremovableBeanBuildItem;
+import io.quarkus.arc.processor.BeanInfo;
 import io.quarkus.arc.processor.BuiltinScope;
 import io.quarkus.deployment.annotations.BuildStep;
+import org.jboss.jandex.MethodInfo;
+
+import java.util.function.Predicate;
 
 class QuarkusProcessor {
 
@@ -11,13 +16,24 @@ class QuarkusProcessor {
     @BuildStep
     AutoAddScopeBuildItem handlersToBeans() {
         return AutoAddScopeBuildItem.builder()
-                .anyMethodMatches(method -> {
-                    return method.name().startsWith("makeMeBean");
-                })
+                .anyMethodMatches(isBeanMaking)
                 .unremovable()
                 .defaultScope(BuiltinScope.APPLICATION)
                 .build();
     }
 
+    @BuildStep
+    UnremovableBeanBuildItem makeHandlersUnremovable() {
+        return new UnremovableBeanBuildItem(new HandlersExclusion());
+    }
+
+    public static class HandlersExclusion implements Predicate<BeanInfo> {
+        @Override
+        public boolean test(BeanInfo bean) {
+            return bean.getImplClazz().methods().stream().anyMatch(isBeanMaking);
+        }
+    }
+
+    static Predicate<MethodInfo> isBeanMaking = method -> method.name().startsWith("makeMeBean");
 
 }
